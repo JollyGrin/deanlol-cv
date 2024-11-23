34213720e93261ad65bd591839998f8cea9a3d19
jollygrin <JollyGrin@protonmail.com>
2024-11-23 12:56:45 +0100

    add responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,29 @@ import { SkillTags } from "@/components/molecules/SkillTag";
 export default function Home() {
   return (
     <main className="px-4 flex flex-col gap-8">
-      <div className="grid place-items-center h-[20vh]">
-        <h1 className="text-7xl">dean</h1>
-      </div>
+      <div className="max-w-[450px] mx-auto">
+        <div className="grid place-items-center h-[20vh]">
+          <h1 className="text-7xl">dean</h1>
+        </div>
 
-      <div className="w-full flex flex-col items-center">
-        <h4 className="underline">dabbles in</h4>
-        <div className="bg-highlight rounded p-2 flex flex-wrap gap-2 w-full">
-          <SkillTags />
+        <div className="w-full flex flex-col items-center">
+          <h4 className="underline">dabbles in</h4>
+          <div className="bg-highlight rounded p-2 flex flex-wrap gap-2 w-full">
+            <SkillTags />
+          </div>
         </div>
       </div>
 
-      <div className="w-full flex flex-col items-center">
-        <h4 className="underline">worked at</h4>
-        <JobRecords />
+      <div className="md:flex md:gap-5 md:justify-center max-w-[900px] mx-auto">
+        <div className="w-full flex flex-col items-center">
+          <h4 className="underline">worked at</h4>
+          <JobRecords />
+        </div>
+
+        <div className="w-full flex flex-col items-center">
+          <h4 className="underline">side projects</h4>
+          <JobRecords />
+        </div>
       </div>
     </main>
   );
