ddc9e0731df2a0ff3744ec04f301a160085d31c7
jollygrin <JollyGrin@protonmail.com>
2024-11-23 23:28:16 +0100

    clean up sideproject

--- src/components/molecules/SideProject/index.tsx
+++ src/components/molecules/SideProject/index.tsx
@@ -6,7 +6,7 @@ import { useRef, useState } from "react";
 
 export const SideProjects = () => {
   return (
-    <div className="w-full grid grid-cols-2 gap-1">
+    <div className="w-full grid grid-cols-1 gap-4">
       {SIDES.map((side, i) => (
         <SideProject key={side.name + i + "side"} {...side} />
       ))}
@@ -18,7 +18,7 @@ const SideProject = (job: Side) => {
   return (
     <div className="relative">
       <div
-        className="relative w-full bg-shadow rounded-lg min-h-[10rem] flex flex-col gap-2 hover:scale-110 hover:z-50 transition-all"
+        className="relative w-full rounded-lg flex flex-col gap-2 hover:scale-110 hover:z-50 transition-all"
         style={{
           backgroundImage: `url("${job.video}")`,
           backgroundSize: "cover",
@@ -31,14 +31,27 @@ const SideProject = (job: Side) => {
         >
           <div className="flex items-center text-xl font-bold font-mono">
             {job.logo === "" ? (
-              <div className="w-[1rem] h-[4rem]" />
+              <div className="w-[2rem] h-[2rem]" />
             ) : (
-              <img src={job.logo} alt="logo" className="w-[4rem] h-[4rem]" />
+              <img src={job.logo} alt="logo" className="w-[2rem] h-[2rem]" />
             )}
             <p>{job.name}</p>
           </div>
-          <div className="p-2">
-            <p>{job.pitch}</p>
+          <div className="">
+            <div className="py-2">
+              <p>{job.pitch}</p>
+            </div>
+            <div className="flex flex-wrap">
+              {job.skills.map((skill, index) => {
+                const isNotLast = index < job.skills.length - 1;
+                return (
+                  <>
+                    <p key={skill + index + job.name}>{skill}</p>
+                    {isNotLast && <span className="mx-2"> *</span>}
+                  </>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
