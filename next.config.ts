6d5f6e2976f5a4d53a0fa15ae57bbcaee8da5cb0
jollygrin <JollyGrin@protonmail.com>
2024-11-24 12:29:33 +0100

    remove basepath when deploying to cvdeanlol

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "out",
-  // Required for GitHub Pages deployment
-  basePath: process.env.NODE_ENV === "production" ? "/deanlol-cv" : "/",
+  // Required for GitHub Pages deployment - turned off when deploying to cv.dean.lol
+  // basePath: process.env.NODE_ENV === "production" ? "/deanlol-cv" : "/",
   images: {
     unoptimized: true,
   },
